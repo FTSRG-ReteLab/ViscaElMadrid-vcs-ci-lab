d6f92bd3f431ee9bdf98508523bd4286a007e8ed
szlnnn <csortanszilard@yahoo.com>
2020-03-29 18:01:50 +0200

    implementing reference speed change in TrainControllerImpl

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,6 +7,19 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Thread t;
+
+	public TrainControllerImpl() {
+		t = new Thread(() -> {
+			t.start();
+			try {
+				followSpeed();
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
 	@Override
 	public void followSpeed() {
